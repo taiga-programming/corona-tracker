aa63df9259dbd8ebfe3149f6676c4a24e1f2d615
taiga <taiga.similarities@gmail.com>
2020-08-13 19:27:42 +0900

    Updates

---
+++ precache-manifest.7790d0e75ba98f413ce5babd30a95513.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2927e4df794d940da380fcd722fbcc20",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "c6d1c1e5a92f3d32d940",
+    "url": "/corona-tracker/static/css/main.600eb2b0.chunk.css"
+  },
+  {
+    "revision": "a834a021c137e4c82a53",
+    "url": "/corona-tracker/static/js/2.b81b545c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.b81b545c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d1c1e5a92f3d32d940",
+    "url": "/corona-tracker/static/js/main.f21e9911.chunk.js"
+  },
+  {
+    "revision": "e89e429ef6a7ab04b2c7",
+    "url": "/corona-tracker/static/js/runtime-main.05a5c543.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker/static/media/image.d7265326.png"
+  }
+]);
